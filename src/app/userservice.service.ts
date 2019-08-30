9063c8a73842f57ae36b576492cc3cf6f70fb7e7
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-08-30 10:12:36 +0200

    the userview class

--- src/app/userservice.service.ts
+++ src/app/userservice.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserserviceService {
 
+  public username:string;
+  public token ="23fc1f308fd98aa75501702a95bdd3c27d7108e5"
   constructor() { }
 }
---
+++ src/app/userview.ts
@@ -0,0 +1,4 @@
+export class Userview {
+    constructor(public username:string, public followers:string, public)
+}
+
