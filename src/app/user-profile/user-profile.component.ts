990c03d8d3f5448b071b00fcd6cc21ea316d0711
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-08-30 12:15:32 +0200

    the userview codes

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Userview } from '../userview';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  user :Userview;
+
+  constructor(private http:HttpClient) { 
+  
+   }
 
   ngOnInit() {
+    interface ApiResponse {
+      login:string;
+      public_repo:number;
+      followers:number;
+      following:number;
+      created_at:Date;
+    }
+    this.http.get<ApiResponse>("https://api.github.com/users/daneden?access_token=23fc1f308fd98aa75501702a95bdd3c27d7108e5").subscribe(data=>{
+      // Succesful API request
+      this.user = new Userview(data.login, data.public_repo, data.followers, data.following, data.created_at)
+    })
   }
 
 }
--- src/app/userview.ts
+++ src/app/userview.ts
@@ -1,4 +1,6 @@
 export class Userview {
-    constructor(public username:string, public followers:string, public)
+    constructor(public username:string, public repositories:number ,public followers:number, public following:number , public creation:Date){
+
+    }
 }
 
