19715e0a7163feb14569ec4c0f1b34f22ae9b3d5
angelicty <muyenziraissa@gmail.com>
2019-09-01 00:56:43 +0200

    displaying services

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -11,7 +11,7 @@ import { Userview } from '../userview';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-    
+
   report: Userview;
   userName :string;
   reponse : any;
@@ -23,23 +23,24 @@ export class FormComponent implements OnInit {
   ngOnInit() {
   }
   search (){
-      this.raissa.userService(this.userName);
-      this.report=this.raissa.user; 
-    //   let promise = new Promise ((resolve , reject) => {
-    // this.http.get('https://api.github.com/users/'+this.userName + "?access_token=23fc1f308fd98aa75501702a95bdd3c27d7108e5")
-    // .subscribe((reponse) => {
-    //   this.reponse = reponse;
-    //   console.log(this.reponse)
-    //   resolve () 
-    // },
-    // err => {
-    //     this.reponse.login="Invalid "
+    this.raissa.userService(this.userName);
+    this.report=this.raissa.user;
+    console.log(this.report) 
+  //   let promise = new Promise ((resolve , reject) => {
+  // this.http.get('https://api.github.com/users/'+this.userName + "?access_token=23fc1f308fd98aa75501702a95bdd3c27d7108e5")
+  // .subscribe((reponse) => {
+  //   this.reponse = reponse;
+  //   console.log(this.reponse)
+  //   resolve () 
+  // },
+  // err => {
+  //     this.reponse.login="Invalid "
 
-    //  reject()
-    // })
+  //  reject()
+  // })
 
-    // })
+  // })
 
-  }
+}
 
 }
--- src/app/userview.spec.ts
+++ src/app/userview.spec.ts
@@ -2,6 +2,6 @@ import { Userview } from './userview';
 
 describe('Userview', () => {
   it('should create an instance', () => {
-    expect(new Userview()).toBeTruthy();
+    expect(new Userview("",0,0,0,"","")).toBeTruthy();
   });
 });
