afeda24b87811c6c55e97f0e87a2c32e5286cf9a
angelicty <muyenziraissa@gmail.com>
2019-08-31 18:01:35 +0200

    adding promises

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { resolve } from 'url';
+import { reject } from 'q';
 
 @Component({
   selector: 'app-form',
@@ -18,12 +20,20 @@ export class FormComponent implements OnInit {
   ngOnInit() {
   }
   search (){
+      let promise = new Promise ((resolve , reject) => {
     this.http.get('https://api.github.com/users/'+this.userName + "?access_token=23fc1f308fd98aa75501702a95bdd3c27d7108e5")
     .subscribe((reponse) => {
       this.reponse = reponse;
       console.log(this.reponse)
-      
+      resolve () 
+    },
+    err => {
+        this.reponse.login="Invalid "
+     reject()
     })
+
+    })
+
   }
 
 }
